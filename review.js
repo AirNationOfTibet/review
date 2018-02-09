1c2f990fb7c33b8164f3332dd96899491aa081f8
Tenzin Chosang <TenzinChosang@Tenzins-MacBook-Air.local>
2018-02-09 10:37:07 -0600

    guitars

--- review.js
+++ review.js
@@ -1,14 +1,21 @@
-console.log("hello")
-
-let guitars = 0;
+let guitars = [];
 
 const MAXCAPACITY = 9;
-
-function newGuitar() {
-  if (guitars < MAXCAPACITY){
-    guitars++;
+//FUNCTION NEWGUITAR CONSUMES ARGUMENT AND CALLS IT GUITARNAME
+function newGuitar(guitarName) {
+  //checking length of the guitars array against the const MAXCAPACITY
+  if (guitars.length < MAXCAPACITY){
+    //HAVE ROOM, PUSH GUITAR NAME INTO GUITARS ARRAY
+    guitars.push(guitarName);
     return true;
   } else {
     return false;
+  }
+}
+
+function showGuitars() {
+  //LOOP THROUGH GUITARS array
+  for (i=0; i < guitars.length; i++){
+    console.log(guitars[i] );
   }
 }
